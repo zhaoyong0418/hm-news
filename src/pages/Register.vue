12b04d5d7dd7240c70917e0468adc21f9aed5388
zhaoyong <505661673@qq.com>
2020-05-09 16:17:26 +0800

    登陆功能-准备工作

--- src/pages/Register.vue
+++ src/pages/Register.vue
@@ -1,5 +1,5 @@
 <template>
-  <router-view></router-view>
+  <div>我是注册组件</div>
 </template>
 
 <script>
@@ -9,5 +9,5 @@ export default {
 </script>
 
 <style>
- 
+
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,19 @@ module.exports = {
     devServer: {
         open: true,
         port: 8888
-    }
+    },
+    css: {
+        loaderOptions: {
+          css: {},
+          postcss: {
+            plugins: [
+              require('postcss-px2rem')({
+                // rem的单位， 因为使用了lib-flexible, lib-flexible会把所有的屏幕分成10份
+                // 1rem 就应该配置为 设计图的  1/10   640
+                remUnit: 37.5
+              })
+            ]
+          }
+        }
+      }
 }
